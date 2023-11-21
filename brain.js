065417c6975079015abb6bb6ec459c72b8dbba82
Caio Marianni <caiomarianni@gmail.com>
2023-11-21 14:39:52 -0300

    toma ai o prototipo do site

---
+++ brain.js
@@ -0,0 +1,28 @@
+// DARK MODE SWITCH--------------------------------------------------
+const $html = document.querySelector('html')
+const $ThemeButton  = document.querySelector('#switch')
+
+$ThemeButton.addEventListener('click', function() {
+    $html.classList.toggle('light-mode')
+})
+
+// NAV ELEMENTS SELECT-----------------------------------------------
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("#navbar .text-container ul li a");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((a ) => {
+    a.classList.remove("active");
+    if (a.classList.contains(current)) {
+      a.classList.add("active");
+    }
+  });
+});
